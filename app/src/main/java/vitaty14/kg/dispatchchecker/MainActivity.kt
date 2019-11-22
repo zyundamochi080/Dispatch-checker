2884e0d227923d65a776c3fac8a2433598bb1b6b
KG <kg.vitaty14@gmail.com>
2019-11-22 17:00:22 +0900

    Initial commit

---
+++ app/src/main/java/vitaty14/kg/dispatchchecker/MainActivity.kt
@@ -0,0 +1,12 @@
+package vitaty14.kg.dispatchchecker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
