3dc19341e58cffe4a35ac5ee3f41eba0686ddcea
KG <kg.vitaty14@gmail.com>
2019-11-23 17:29:20 +0900

    [WIP]Add timer system

--- app/src/main/java/vitaty14/kg/dispatchchecker/MainActivity.kt
+++ app/src/main/java/vitaty14/kg/dispatchchecker/MainActivity.kt
@@ -1,12 +1,38 @@
 package vitaty14.kg.dispatchchecker
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        buttonStart1.setOnClickListener {
+            val alertTime = System.currentTimeMillis() + 5000
+            val alarmIntent = Intent(this,AlarmReceiver::class.java)
+            val pendingIntent = PendingIntent.getBroadcast(this,0,alarmIntent,PendingIntent.FLAG_UPDATE_CURRENT)
+
+            val manager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            manager.setExact(AlarmManager.RTC_WAKEUP , alertTime ,pendingIntent)
+        }
+    }
+}
+
+class AlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Toast.makeText(context,"hoge",Toast.LENGTH_SHORT).show()
     }
 }
