8afed38c9ce235aa3da27b84ed5c07df4d37aeaf
KG <kg.vitaty14@gmail.com>
2019-11-23 18:32:27 +0900

    Add notifycation

--- app/src/main/java/vitaty14/kg/dispatchchecker/MainActivity.kt
+++ app/src/main/java/vitaty14/kg/dispatchchecker/MainActivity.kt
@@ -1,13 +1,16 @@
 package vitaty14.kg.dispatchchecker
 
 import android.app.AlarmManager
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import com.deploygate.sdk.DeployGate
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -33,7 +36,24 @@ class MainActivity : AppCompatActivity() {
 }
 
 class AlarmReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
+    override fun onReceive(context: Context, intent: Intent) {
         Toast.makeText(context,"hoge",Toast.LENGTH_LONG).show()
+        DeployGate.logDebug("onReceive")
+
+        val notifyIntent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(context, 0, notifyIntent, 0)
+
+        val notification= NotificationCompat.Builder(context)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setAutoCancel(true)
+            .setContentTitle("Test")
+            .setContentText("content text")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .build()
+        val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        manager.notify(1, notification)
     }
 }
